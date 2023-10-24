a4d527a53c705a5c5cda29805684784895f2cb20
Sergey-8057 <erste_sergey@mail.ru>
2023-10-25 01:38:32 +0300

    create_lesson-12

---
+++ Lesson_12/src/database/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Date, Integer, String, func
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+
+
+class Human(Base):
+    __tablename__ = "humans"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    surname = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True)
+    birthday = Column(Date)
+    description = Column(String, default=None)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", back_populates="humans")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    refresh_token = Column(String(255), nullable=True)
+    humans = relationship("Human", back_populates="user")
---
+++ Lesson_12/src/routes/humans.py
@@ -0,0 +1,90 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status, Path, Query
+from sqlalchemy.orm import Session
+
+from src.database.connect import get_db
+from src.database.models import User
+from src.schemas import ResponseHuman, HumanModel
+from src.repository import humans as repository_humans
+from src.services.auth import auth_service
+
+
+router = APIRouter(prefix='/humans', tags=['Humans'])
+
+
+@router.get("/", response_model=List[ResponseHuman])
+async def get_humans(limit: int = Query(10, le=100), offset: int = 0, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    humans = await repository_humans.get_humans(limit, offset, current_user, db)
+    return humans
+
+
+@router.get("/{human_id}", response_model=ResponseHuman)
+async def get_human(human_id: int = Path(ge=1), db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.get_human(human_id, current_user, db)
+    if human is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
+
+
+@router.get("/search_name/", response_model=List[ResponseHuman])
+async def get_human_name(human_name: str = Query(title="Name"), db: Session = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.get_human_name(human_name, current_user, db)
+    if not human:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
+
+
+@router.get("/search_surname/", response_model=List[ResponseHuman])
+async def get_human_surname(human_surname: str = Query(title="Surname"), db: Session = Depends(get_db),
+                            current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.get_human_surname(human_surname, current_user, db)
+    if not human:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
+
+
+@router.get("/search_email/", response_model=List[ResponseHuman])
+async def get_human_email(human_email: str = Query(title="Email"), db: Session = Depends(get_db),
+                        current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.get_human_email(human_email, current_user, db)
+    if not human:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
+
+
+@router.get("/list_birthday/", response_model=List[ResponseHuman])
+async def get_list_birthdays(db: Session = Depends(get_db),
+                            current_user: User = Depends(auth_service.get_current_user)):
+    humans = await repository_humans.get_list_birthdays(current_user, db)
+    if not humans:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return humans
+
+
+@router.post("/", response_model=ResponseHuman, status_code=status.HTTP_201_CREATED)
+async def create_human(body: HumanModel, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.create_human(body, current_user, db)
+    return human
+
+
+@router.put("/{human_id}", response_model=ResponseHuman)
+async def update_human(body: HumanModel, human_id: int = Path(ge=1), db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.update_human(body, human_id, current_user, db)
+    if human is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
+
+
+@router.delete("/{human_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_human(human_id: int = Path(ge=1), db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    human = await repository_humans.remove_human(human_id, current_user, db)
+    if human is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not Found")
+    return human
